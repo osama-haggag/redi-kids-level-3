45c243ed012a629d9cff2e40454ae010686505aa
Osama Haggag <osama.haggag@gmail.com>
2019-09-19 10:54:55 +0200

    Add initial number guessing game

--- flaskberrypi/flask_server/flask_app.py
+++ flaskberrypi/flask_server/flask_app.py
@@ -1,15 +1,38 @@
-from flask import Flask, request, render_template
+import random
+
+from flask import Flask, request, render_template, session
 
 app = Flask(__name__)
+app.secret_key = 'thisIsSecret'
+
+MAX_ATTEMPTS = 7
 
 
-@app.route('/move', methods=['POST'])
-def move():
-    return render_template('index.html')
+@app.route('/guess', methods=['POST'])
+def guess():
+    if session['num_attempts'] == 0:
+        return "You failed!"
+    user_guess_input = request.form.get('guess')
+    if not user_guess_input.isnumeric():
+        return "Wrong Input: give me a number"
+
+    user_guess = int(user_guess_input)
+
+    if user_guess == session['secret_number']:
+        return render_template('index.html', answer='Correct')
+    elif user_guess > session['secret_number']:
+        session['num_attempts'] -= 1
+        return render_template('index.html', answer='Too High. {} attempts left!'.format(session['num_attempts']))
+    else:
+        session['num_attempts'] -= 1
+        return render_template('index.html', answer='Too Low. {} attempts left!'.format(session['num_attempts']))
 
 
 @app.route('/')
 def main():
+    session['secret_number'] = random.randint(1, 100)
+    print(session['secret_number'])
+    session['num_attempts'] = MAX_ATTEMPTS
     return render_template('index.html')
 
 
