3228ff31c10ae449348bef47538b72969d525754
Tom Quinonero <tq@3yourmind.com>
2019-09-21 16:19:52 +0200

    fix

--- flaskberrypi/pins.py
+++ flaskberrypi/pins.py
@@ -14,8 +14,16 @@ leds = {
 }
 
 def displayLife(leds, lifeLeft):
+  for led_index in range(len(leds)):
+    leds[led_index].off()
   for led_index in range(lifeLeft):
     leds[led_index].on()
 
+displayLife(leds, 9)
+time.sleep(5)
 
-displayLife(leds,9)
+displayLife(leds, 5)
+time.sleep(5)
+
+displayLife(leds, 3)
+time.sleep(5)
